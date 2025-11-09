24b675a86aad820f833a22771c11f5899159ddf5
forfi4 <liamipomi@gmail.com>
2025-11-09 16:47:45 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,15 +1,25 @@
 def move(my_list, direction):
+    try:
+        current_index = my_list.index(1)
+    except ValueError:
+        return my_list
 
-    # Finds the index of the one in the list
-    index_of_one = my_list.index(1)
+    list_length = len(my_list)
 
-    # Move the one to the left or to the right
     if direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
-
+        if current_index < list_length - 1:
+            # Move the pig right
+            my_list[current_index] = 0
+            my_list[current_index + 1] = 1
     elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
-
+        if current_index > 0:
+            # Move the pig left
+            my_list[current_index] = 0
+            my_list[current_index - 1] = 1
+            
     return my_list
+print(f"move([0, 0, 0, 1, 0], 'right'): {move([0, 0, 0, 1, 0], 'right')}")
+print(f"move([0, 1, 0, 0, 0], 'left'):  {move([0, 1, 0, 0, 0], 'left')}")
+
+print(f"move([1, 0, 0, 0, 0], 'left'):  {move([1, 0, 0, 0, 0], 'left')}")
+print(f"move([0, 0, 0, 0, 1], 'right'): {move([0, 0, 0, 0, 1], 'right')}")
